b2c6b5dcf2de4569b7117577e8114bd4468deab8
yokeev <keviv1q2alt@gmail.com>
2023-10-05 21:01:16 -0500

    Add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Sistema de gestión de proyectos - Documentación')
+    .setDescription(
+      'Documentación del sistema de gestión de proyectos - Empresa WVS EIRL',
+    )
+    .setVersion('1.0')
+    .addTag('projects')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
